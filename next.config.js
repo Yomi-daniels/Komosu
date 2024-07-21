383b9d11117c748630a4289d0136f3e708deb10d
YODA <121935558+Yomi-daniels@users.noreply.github.com>
2024-07-21 12:07:39 +0100

    initial commit

---
+++ next.config.js
@@ -0,0 +1,25 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   reactStrictMode: true,
+// };
+// module.exports = {
+//   //...
+//   resolve: {
+//     extensions: [".js", ".jsx"],
+//   },
+// };
+// module.exports = nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
