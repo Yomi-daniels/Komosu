9c9a718ec3ba4d4c0f1eb45749ffa793d18c29bc
YODA <121935558+Yomi-daniels@users.noreply.github.com>
2024-07-28 22:33:54 +0100

    updating media queries

--- src/app/case-study/casestudycontact/CaseStudyContactLink.jsx
+++ src/app/case-study/casestudycontact/CaseStudyContactLink.jsx
@@ -2,19 +2,24 @@ import casestyles from "../casestudy.module.css";
 import Link from "next/link";
 import Image from "next/image";
 const CaseStudyContactLink = () => {
-  return <section className={casestyles.contactLinksection}>
-        <div className={casestyles.contactLinkContainer}>
+  return (
+    <section className={casestyles.contactLinksection}>
+      <div className={casestyles.contactLinkContainer}>
         <div className={casestyles.topimgabsolute}>
-            <Image src="/Group 2.png" fill/>
+          <Image src="/Group 2.png" fill />
         </div>
-            <h4>Transform Your Sales with Our Proven Lead <br /> Generation Solutions</h4>
-            <Link href="/contact"><button>Contact us</button></Link>
-            <div className={casestyles.bottomimgabsolute}>
-            <Image src="/Group 2.png" fill/>
+        <h4>
+          Transform Your Sales with Our Proven Lead <br /> Generation Solutions
+        </h4>
+        <Link href="/request-demo">
+          <button>Get Started</button>
+        </Link>
+        <div className={casestyles.bottomimgabsolute}>
+          <Image src="/Group 2.png" fill />
         </div>
-        </div>
-  </section>;
-
+      </div>
+    </section>
+  );
 };
 
 export default CaseStudyContactLink;
