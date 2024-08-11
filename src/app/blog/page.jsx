9c7f31ec9b1d9e47aeaef854e37fc283d1492890
lbhMichael <michael0ndukwe@gmail.com>
2024-08-11 01:43:22 +0100

    refactor: remove hardcoded blog posts from BlogPage component

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -2,11 +2,6 @@
 import React from "react";
 import BlogPost from "./components/blogPost";
 
-const posts = [
-  { title: "First Post", content: "This is the content of the first post." },
-  { title: "Second Post", content: "This is the content of the second post." },
-];
-
 const BlogPage = () => {
   return <BlogPost posts={posts} />;
 };
