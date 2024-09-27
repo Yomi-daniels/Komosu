6d65f2d2d46f752fadf8c067dd7e3a89be29a143
YODA <121935558+Yomi-daniels@users.noreply.github.com>
2024-09-27 12:21:30 +0100

    modified script

--- src/Script.jsx
+++ src/Script.jsx
@@ -3,11 +3,17 @@ import Script from "next/script";
 const MyScript = () => {
   return (
     <div>
-      <script src="https://cdn.botpress.cloud/webchat/v1/inject.js"></script>
-      <script
+      {/* External script injected asynchronously */}
+      <Script
+        src="https://cdn.botpress.cloud/webchat/v1/inject.js"
+        strategy="afterInteractive" // Ensure it's loaded after the page becomes interactive
+      />
+
+      {/* Another external script with defer behavior */}
+      <Script
         src="https://mediafiles.botpress.cloud/935ec3d1-76db-4b99-ae26-db94d2a39025/webchat/config.js"
-        defer
-      ></script>
+        strategy="afterInteractive" // Defer loading until after the page is interactive
+      />
     </div>
   );
 };
