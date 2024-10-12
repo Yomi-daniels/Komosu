e132d1c0fcac8c1ea214cd5e41e246eddd2b59cc
YODA <121935558+Yomi-daniels@users.noreply.github.com>
2024-10-12 18:52:34 +0100

    edited leadgen page animation

--- src/app/leadgeneration/leadgenlandingpage/LeadgenLandingpage.jsx
+++ src/app/leadgeneration/leadgenlandingpage/LeadgenLandingpage.jsx
@@ -4,12 +4,34 @@ import Image from "next/image";
 import LandingPageButtons from "@/app/webdesign/components/LandingPageButtons";
 import { Montserrat } from "next/font/google";
 import { ScrollParallax } from "react-just-parallax";
+import { useEffect, useRef } from "react";
+import gsap from "gsap";
+import { TextGenerateEffect } from "@/app/components/ui/text-generate-effect";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
   weight: ["700"],
 });
 const LeadgenLandingpage = () => {
+  const leadGenTextContainerRef = useRef(null);
+  const leadGenaImageContainerRef = useRef(null);
+
+  useEffect(() => {
+    if (leadGenTextContainerRef.current)
+      gsap.fromTo(
+        leadGenTextContainerRef.current,
+        { x: -300, opacity: 0 },
+        { x: 0, opacity: 1, duration: 1.2, ease: "power3.out" }
+      );
+    if (leadGenaImageContainerRef.current)
+      gsap.fromTo(
+        leadGenaImageContainerRef.current,
+        { y: 300, opacity: 0 },
+        { y: 0, opacity: 1, duration: 2, ease: "power3.out" }
+      );
+  }, []);
+  const leadGenHeaderText = `  Drive more leads and transform your dealership growth with our
+            expert solutions`;
   return (
     <section className={leadStyles.LeadingGenSection}>
       <div className={leadStyles.LeadGenContainer}>
@@ -18,6 +40,7 @@ const LeadgenLandingpage = () => {
       </div> */}
         <div
           className={`${leadStyles.leadGenTextContainer} ${montserrat.className}`}
+          ref={leadGenTextContainerRef}
         >
           <div className={leadStyles.leadtextheader}>
             <div className={leadStyles.textheaderimg}>
@@ -26,12 +49,17 @@ const LeadgenLandingpage = () => {
             <h4>LEAD GENERATION</h4>
           </div>
           <h1>
-            Drive more leads and transform your dealership growth with our
-            expert solutions
+            <TextGenerateEffect
+              words={leadGenHeaderText}
+              messageClass="text-[32px] max-sm:text-[32px]"
+            />
           </h1>
           <LandingPageButtons />
         </div>
-        <div className={leadStyles.leadGenaImageContainer}>
+        <div
+          className={leadStyles.leadGenaImageContainer}
+          ref={leadGenaImageContainerRef}
+        >
           <ScrollParallax isAbsolutelyPositioned>
             <div className={leadStyles.leadimgstat}>
               <Image src="/Stat (4).png" fill objectFit="contain" />
--- src/app/leadgeneration/leadgenservices/leadGenServices.jsx
+++ src/app/leadgeneration/leadgenservices/leadGenServices.jsx
@@ -6,12 +6,109 @@ import Testimonials from "@/sections/testimonials";
 import { ScrollParallax } from "react-just-parallax";
 import FAQ from "@/sections/faq";
 import Link from "next/link";
+import { useEffect, useRef } from "react";
+gsap.registerPlugin(ScrollTrigger);
+import { ScrollTrigger } from "gsap/all";
+import gsap from "gsap";
 const leadGenServices = () => {
+  const leadGenServicesContentText1Ref = useRef(null);
+  const leadGenServicesContentText2Ref = useRef(null);
+  const leadGenServicesContentImag1eRef = useRef(null);
+  const leadGenServicesContentImag2eRef = useRef(null);
+  const servicesContactSectioneRef = useRef(null);
+
+  useEffect(() => {
+    //Animate leadGenServicesContentText1Ref from the left
+    gsap.fromTo(
+      leadGenServicesContentText1Ref.current,
+      { x: -300, opacity: 0 }, //start from left
+      {
+        x: 0,
+        opacity: 1,
+        duration: 1.2,
+        ease: "power3.out",
+        scrollTrigger: {
+          trigger: leadGenServicesContentText1Ref.current,
+          start: "top 80%", //start animation when top of the element is 80% from the top of the viewport
+          once: true, //only animate once
+        },
+      }
+    );
+    //Animate leadGenServicesContentText2Ref from the right
+    gsap.fromTo(
+      leadGenServicesContentText2Ref.current,
+      { x: 300, opacity: 0 }, //start from right
+      {
+        x: 0,
+        opacity: 1,
+        duration: 1.2,
+        ease: "power3.out",
+        scrollTrigger: {
+          trigger: leadGenServicesContentText2Ref.current,
+          start: "top 80%", //start animation when top of the element is 80% from the top of the viewport
+          once: true, //only animate once
+        },
+      }
+    );
+    //Animate leadGenServicesContentImag1eRef from the right
+    gsap.fromTo(
+      leadGenServicesContentImag1eRef.current,
+      { x: 300, opacity: 0 }, //start from right
+      {
+        x: 0,
+        opacity: 1,
+        duration: 1.2,
+        ease: "power3.out",
+        scrollTrigger: {
+          trigger: leadGenServicesContentImag1eRef.current,
+          start: "top 80%", //start animation when top of the element is 80% from the top of the viewport
+          once: true, //only animate once
+        },
+      }
+    );
+    //Animate leadGenServicesContentImag2eRef from the left
+    gsap.fromTo(
+      leadGenServicesContentImag2eRef.current,
+      { x: -300, opacity: 0 }, //start from left
+      {
+        x: 0,
+        opacity: 1,
+        duration: 1.2,
+        ease: "power3.out",
+        scrollTrigger: {
+          trigger: leadGenServicesContentImag2eRef.current,
+          start: "top 80%", //start animation when top of the element is 80% from the top of the viewport
+          once: true, //only animate once
+        },
+      }
+    );
+
+    if (servicesContactSectioneRef.current)
+      gsap.fromTo(
+        servicesContactSectioneRef.current,
+        { y: 200, opacity: 0 },
+        {
+          y: 0,
+          opacity: 1,
+          duration: 1,
+          ease: "power3.out",
+          scrollTrigger: {
+            trigger: servicesContactSectioneRef.current,
+            start: "top 100%", //start animation when top of the element is 100% from the top of the viewport
+            once: true, //only animate once
+          },
+        }
+      );
+  }, []);
+
   return (
     <section className={leadStyles.leadServicesSection}>
       <div className={leadStyles.leadGenServicesContainer}>
         <div className={leadStyles.leadGenServicesContent}>
-          <div className={leadStyles.leadGenServicesContentText}>
+          <div
+            className={leadStyles.leadGenServicesContentText}
+            ref={leadGenServicesContentText1Ref}
+          >
             <h4>
               Seamlessly integrate our AI with your CRM system to manage and
               track leads.
@@ -20,7 +117,10 @@ const leadGenServices = () => {
               <GetStarted />
             </div>
           </div>
-          <div className={leadStyles.leadGenServicesContentImage}>
+          <div
+            className={leadStyles.leadGenServicesContentImage}
+            ref={leadGenServicesContentImag1eRef}
+          >
             <div className={leadStyles.leadImageWrapper}>
               <div className={leadStyles.leadImgabsolute}>
                 <Image src="/Stat (4).png" fill objectFit="contain" />
@@ -38,12 +138,18 @@ const leadGenServices = () => {
           </div>
         </div>
         <div className={leadStyles.leadGenServicesContent}>
-          <div className={leadStyles.leadGenServicesContentImage}>
+          <div
+            className={leadStyles.leadGenServicesContentImage}
+            ref={leadGenServicesContentImag2eRef}
+          >
             <div className={leadStyles.leadservicesImg}>
               <Image src="/Frame 1618868497.png" fill objectFit="contain" />
             </div>
           </div>
-          <div className={leadStyles.leadGenServicesContentText}>
+          <div
+            className={leadStyles.leadGenServicesContentText}
+            ref={leadGenServicesContentText2Ref}
+          >
             <h4>Provide real-time assistance on your dealership’s website.</h4>
             <div className={leadStyles.GetStartedbtn}>
               <GetStarted />
@@ -51,7 +157,10 @@ const leadGenServices = () => {
           </div>
         </div>
       </div>
-      <section className={leadStyles.servicesContactSection}>
+      <section
+        className={leadStyles.servicesContactSection}
+        ref={servicesContactSectioneRef}
+      >
         <div className={leadStyles.servicesContactContainer}>
           <div className={leadStyles.servicesContactImgtop}>
             <Image src="/Group 6327.svg" fill />
