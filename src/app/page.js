ba88054406bc8f0109d6282c0dace6f5638bc40e
Your Name <your.email@example.com>
2025-03-04 09:07:43 +0100

    removed landing page video content

--- src/app/page.js
+++ src/app/page.js
@@ -8,6 +8,7 @@ import VideoShowcase from "@/sections/videoShowcase";
 import styles from "./page.module.css";
 import Navbar from "./components/navbar/Navbar";
 import WhyKomosu from "@/sections/WhyKomosu";
+import StackedCard from "./dealership_hub/StackedCard";
 // Import `cn` if you need it for conditional class names
 // import cn from "classnames";
 
@@ -23,17 +24,17 @@ const Home = () => {
         <section className={styles.Servisesflex}></section>
       </section>
       <section className={styles.VideoShowcasePage}>
-        <VideoShowcase />
+        <StackedCard />
       </section>
       <section className={styles.OfferPage}>
         <Offer />
       </section>
-   
+
       <section className={styles.OfferAnimationPage}>
         <OfferAnimation />
       </section>
       <section className={styles.OfferAnimationPage}>
-      <WhyKomosu/> 
+        <WhyKomosu />
       </section>
       <section className={styles.TestimonialPage}>
         <Testimonials />
