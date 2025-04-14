f22c10cf1122e6afcaa5df86d841f259179640f5
Your Name <your.email@example.com>
2025-04-14 19:09:46 +0100

    added mobile responsiveness

--- src/app/content/layout.jsx
+++ src/app/content/layout.jsx
@@ -8,7 +8,7 @@ export default function ContentLayout({ children }) {
       <div
         className="lg:ml-[32rem] p-8 w-full h-[100%] mt-36 max-w-[831px] relative overflow-hidden
      sm:ml-[15rem]   
-     max-sm:ml-[1rem]
+     max-sm:ml-[1.7rem]
      max-sm:px-0
      max-sm:w-[600px]
     max-sm:mt-[17rem]
