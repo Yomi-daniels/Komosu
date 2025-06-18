26392936070bbe805bf53d8168c5b81dfbf95e3f
lbhMichael <michael0ndukwe@gmail.com>
2025-06-18 10:13:13 +0100

    added subscription

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -20,6 +20,13 @@ const initDb = async () => {
     )
   `);
 
+  await db.exec(`
+    CREATE TABLE IF NOT EXISTS subscriptions (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      email TEXT NOT NULL UNIQUE,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )
+  `);
   return db;
 };
 
@@ -69,6 +76,39 @@ export const insertContact = async ({
   }
 };
 
+export const insertSubscription = async ({ email }: { email: string }) => {
+  const db = await initDb();
+  try {
+    const result = await db.run(
+      `
+      INSERT INTO subscriptions (email)
+      VALUES (?)
+    `,
+      email
+    );
+    console.log(
+      "Insert subscription executed successfully, lastID:",
+      result.lastID
+    );
+  } catch (error) {
+    if (
+      error.code === "SQLITE_CONSTRAINT" &&
+      error.message.includes("UNIQUE")
+    ) {
+      throw new Error("Email already subscribed");
+    }
+    throw error;
+  } finally {
+    await db.close();
+  }
+};
+
+export const getAllSubscriptions = async () => {
+  const db = await initDb();
+  const rows = await db.all("SELECT * FROM subscriptions");
+  await db.close();
+  return rows;
+};
 // Optional: Function to retrieve all contacts (for admin purposes)
 export const getAllContacts = async () => {
   const db = await initDb();
