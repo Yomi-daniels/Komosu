b6fa3e30d13cf47afab5bd8fe81c97092d43aaa2
yomi daniels <danieleyebiokin7@gmail.com>
2025-06-25 23:02:18 +0100

    added loading ui

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,26 +1,23 @@
+import Image from "next/image";
 import styles from "./page.module.css";
 
 const Loading = ({ className }) => {
   return (
     <div className={`${styles.loading} ${className || ""}`}>
-      <div className={styles.loader}>
-        <div className={styles.circle}>
-          <div className={styles.dot}></div>
-          <div className={styles.outline}></div>
-        </div>
-        <div className={styles.circle}>
-          <div className={styles.dot}></div>
-          <div className={styles.outline}></div>
-        </div>
-        <div className={styles.circle}>
-          <div className={styles.dot}></div>
-          <div className={styles.outline}></div>
-        </div>
-        <div className={styles.circle}>
-          <div className={styles.dot}></div>
-          <div className={styles.outline}></div>
-        </div>
-      </div>
+    <div className={styles.loader}>
+  {[...Array(4)].map((_, index) => (
+    <div key={index} className={styles.tyre}>
+      <Image
+        src="/mdi--tyre.svg" // make sure this file exists in /public
+        alt="Loading Tyre"
+        width={40}
+        height={40}
+        className={styles.tyreIcon}
+      />
+    </div>
+  ))}
+</div>
+
     </div>
   );
 };
