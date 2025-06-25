09163a53857b0cdde09cf9690ba67d53ed55e5f6
yomi daniels <danieleyebiokin7@gmail.com>
2025-06-25 22:24:48 +0100

    changes

--- src/app/webdesign/page.jsx
+++ src/app/webdesign/page.jsx
@@ -223,14 +223,14 @@ const WebDesign = () => {
         </div>
 
         {/* See More / See Less Button */}
-        {filteredVehicles.length > 6 && (
+        {/* {filteredVehicles.length > 6 && (
           <button
             onClick={toggleShowAll}
             className="mt-6 px-6 py-2 bg-blue-600 text-white rounded-full hover:bg-blue-700 transition"
           >
             {showAll ? "See Less" : "See More"}
           </button>
-        )}
+        )} */}
       </div>
 
       <Testimonials />
